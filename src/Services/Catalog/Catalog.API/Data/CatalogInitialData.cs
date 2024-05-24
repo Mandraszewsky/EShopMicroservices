2022d1d00864bba3058dd44607090f460b9a4875
Mandraszewsky <mandrah@o2.pl>
2024-05-24 13:27:30 +0200

    Catalog microservice and BuildingBlocks with Marter, Carter, Mapster, MediatR and FluentValidation.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogInitialData.cs
@@ -0,0 +1,58 @@
+﻿using Marten.Schema;
+
+namespace Catalog.API.Data;
+
+public class CatalogInitialData : IInitialData
+{
+    public async Task Populate(IDocumentStore store, CancellationToken cancellation)
+    {
+        using var session = store.LightweightSession();
+
+        if (await session.Query<Product>().AnyAsync())
+            return;
+
+        session.Store<Product>(GetPreconfiguredProducts());
+        await session.SaveChangesAsync();
+    }
+
+    private static IEnumerable<Product> GetPreconfiguredProducts() => new List<Product>()
+    {
+        new Product()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Product #1",
+            Description = "Description #1",
+            ImageFile = "item-1.png",
+            Price = 100.00M,
+            Category = new List<string> { "category #1" }
+        },
+        new Product()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Product #2",
+            Description = "Description #2",
+            ImageFile = "item-2.png",
+            Price = 200.00M,
+            Category = new List<string> { "category #1", "category #2" }
+        },
+        new Product()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Product #3",
+            Description = "Description #3",
+            ImageFile = "item-3.png",
+            Price = 300.00M,
+            Category = new List<string> { "category #2", "category #3" }
+        },
+        new Product()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Product #4",
+            Description = "Description #4",
+            ImageFile = "item-4.png",
+            Price = 400.00M,
+            Category = new List<string> { "category #4" }
+        }
+    };
+
+}
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,45 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly;
+
+//Add services:
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+if (builder.Environment.IsDevelopment())
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
+var app = builder.Build();
+
+//Congifure the HTTP request pipeline:
+app.MapCarter();
+
+app.UseExceptionHandler(opt => { });
+
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.Run();
